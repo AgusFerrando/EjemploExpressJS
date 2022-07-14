ac43ca07d36be6399f91a46525727ffd1d11d901
AgusFerrando <104893201+AgusFerrando@users.noreply.github.com>
2022-07-14 10:02:43 +0200

    ejemplo

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch')
+var express = require('express');
+var router = express.Router();
+
+let listaItems = [
+  item1= 'item1',
+  item2= 'item2',
+  item3= 'item3'
+]
+
+let pokeList = []
+
+
+fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+    .then(response => response.json())
+    .then(res => {
+      for (let i of res.results) {
+        fetch(i.url)
+          .then(x => x.json())
+          .then(x => {
+            pokeList.push({"name": x.name, "height":x.height, "image":x.sprites.front_default}); 
+          })
+        }                                          
+      })
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { listaItems , pokeList });
+});
+
+router.get('/cards', function(req, res, next) {
+  res.render('cards', { pokeList });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+
+module.exports = router;
